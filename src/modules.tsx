88d21616e5b0b6e9127e62e8a348608fce20e915
Matt Hova <matthova87@gmail.com>
2023-10-19 22:10:31 -0700

    Update API to pass in an object with k: theme name and v: corresponding class name

--- src/modules.tsx
+++ src/modules.tsx
@@ -1,21 +1,38 @@
 import * as React from "react";
 
-export function createHelper<T extends string>(themeNameArray: T[]) {
+type ThemeName = 'dark' | 'light';
+type ClassName = string;
+type ThemeObject = Record<ThemeName, ClassName>;
+
+export function createHelper<T extends ThemeObject>(themeObject: T) {
+  type ThemeClassName = T[Extract<keyof T, string>]
   function nullishStringToThemeName(
     themeNameString?: string | null
-  ): T | undefined {
-    for (const themeName of themeNameArray) {
-      if (themeName === themeNameString) return themeName;
+  ): keyof T | undefined {
+    if (themeNameString == null) return undefined;
+    for (const themeName in themeObject) {
+      if (themeName === themeNameString) {
+        return themeName
+      }
+    }
+    return undefined;
+  }
+  function getThemeClassName(
+    themeName?: keyof T | null
+  ): ThemeClassName | undefined {
+    if (themeName == null) return undefined;
+    for (const themeName in themeObject) {
+      if (themeName === themeName) {
+        return themeObject[themeName]
+      }
     }
     return undefined;
   }
 
   /**
-   *  We are assuming the default browser theme name is "light"
-   *  Note: we're returning T | undefined even though this function will only
-   *  ever return 'light' | 'dark' | undefined
+   *  Note: We are assuming the default browser theme name is "light"
    */
-  function getOsThemeName(): T | undefined {
+  function getOsThemeName(): ThemeName | undefined {
     if (
       typeof window === "undefined" ||
       typeof window.matchMedia === "undefined"
@@ -23,35 +40,37 @@ export function createHelper<T extends string>(themeNameArray: T[]) {
       return undefined;
     }
     if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-      return "dark" as T;
+      return "dark";
     }
-    return "light" as T;
+    return "light";
   }
 
   type ThemeContextValue = {
-    themeName: T | undefined;
-    osThemeName: T | undefined;
-    setThemeName: React.Dispatch<React.SetStateAction<T | undefined>>;
+    themeName: keyof T | undefined;
+    themeClassName: T[keyof T] | undefined;
+    osThemeName: keyof T | undefined;
+    setThemeName: React.Dispatch<React.SetStateAction<keyof T | undefined>>;
   };
 
   const ThemeContext = React.createContext<ThemeContextValue>({
     themeName: undefined,
+    themeClassName: undefined,
     osThemeName: undefined,
     // eslint-disable-next-line @typescript-eslint/no-empty-function
-    setThemeName: () => {},
+    setThemeName: () => { },
   });
 
   const useThemeInfoHook = (initialThemeName?: string) => {
-    const [themeName, setThemeName] = React.useState<T | undefined>(
+    const [themeName, setThemeName] = React.useState<keyof T | undefined>(
       nullishStringToThemeName(initialThemeName)
     );
-    const [osThemeName, setOsThemeName] = React.useState<T | undefined>(
+    const [osThemeName, setOsThemeName] = React.useState<keyof T | undefined>(
       getOsThemeName()
     );
 
     React.useEffect(() => {
       function handleThemeChange(e: MediaQueryListEvent) {
-        const newOsThemeName = (e.matches ? "dark" : "light") as T;
+        const newOsThemeName = (e.matches ? "dark" : "light");
         setOsThemeName(newOsThemeName);
       }
 
@@ -65,7 +84,13 @@ export function createHelper<T extends string>(themeNameArray: T[]) {
       };
     }, []);
 
-    return { themeName, setThemeName, osThemeName };
+    const activeThemeName = themeName ?? osThemeName;
+    let themeClassName: T[keyof T] | undefined = undefined;
+    if (activeThemeName != null && themeObject.hasOwnProperty(activeThemeName)) {
+      themeClassName = themeObject[activeThemeName]
+    }
+
+    return { themeName, setThemeName, osThemeName, themeClassName };
   };
 
   function ThemeProvider({
@@ -73,7 +98,7 @@ export function createHelper<T extends string>(themeNameArray: T[]) {
     themeName,
   }: {
     children: React.ReactNode;
-    themeName: T | undefined;
+    themeName: string;
   }) {
     const value = useThemeInfoHook(themeName);
     return (
@@ -90,6 +115,6 @@ export function createHelper<T extends string>(themeNameArray: T[]) {
     ThemeContext,
     useThemeInfo,
     ThemeProvider,
-    nullishStringToThemeName,
+    getThemeClassName,
   };
 }
